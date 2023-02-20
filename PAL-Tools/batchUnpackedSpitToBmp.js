7419970f6f9d3f6ef1cc00914869788131ab1349
Abdul-Haseeb Ahmad <abdul.ahmad@gmail.com>
2023-02-20 15:57:38 -0500

    WIP Alpha 0.4

--- PAL-Tools/batchUnpackedSpitToBmp.js
+++ PAL-Tools/batchUnpackedSpitToBmp.js
@@ -5,10 +5,10 @@ const execSync = require('child_process').execSync;
 const root = './Unpack/NHL95QFS';
 
 const palMapDef = {
-  CREDIT: ['CREDIT00', 'CREDIT01', 'CREDIT02', 'CREDIT03', 'CREDIT04', 'CREDIT05', 'CREDIT06', 'CREDIT07', 'CREDIT08', 'CREDIT09', 'CREDIT10', 'CREDIT11', 'CREDIT12', 'CREDIT13', 'CREDIT14', 'CREDIT15', 'CREDIT16', 'CREDIT17', 'CREDIT18', 'CREDIT19'],
+  CREDITS: ['CREDIT00', 'CREDIT01', 'CREDIT02', 'CREDIT03', 'CREDIT04', 'CREDIT05', 'CREDIT06', 'CREDIT07', 'CREDIT08', 'CREDIT09', 'CREDIT10', 'CREDIT11', 'CREDIT12', 'CREDIT13', 'CREDIT14', 'CREDIT15', 'CREDIT16', 'CREDIT17', 'CREDIT18', 'CREDIT19'],
   HOMEPAL3: ['CTLOGO', 'SRLOGO'],
   CITY: ['CTTITLE','CTTITLE1','CTTITLE2','CTTITLE3','PSTATBAR'],
-  DB_DBUT: ['DIALOGBX','EXHGAME2', 'PLAQUE', 'PLAYOFFS', 'SETTING', 'SETTINGS', 'SETTING2', 'SETTING3', 'SETTING4', 'SETTING5', 'SETTING6', 'SETTING7' ],
+  DB_BUT: ['DIALOGBX','EXHGAME2', 'PLAQUE', 'PLAYOFFS', 'SETTING', 'SETTINGS', 'SETTING2', 'SETTING3', 'SETTING4', 'SETTING5', 'SETTING6', 'SETTING7' ],
   EMBPAL: ['EMBANA', 'EMBASE', 'EMBASW', 'EMBBOS', 'EMBBUF', 'EMBCGY', 'EMBCHI', 'EMBDAL', 'EMBDET', 'EMBEAS', 'EMBEDM', 'EMBFLA', 'EMBLA', 'EMBMTL', 'EMBNHL', 'EMBNJ', 'EMBNYI', 'EMBNYR', 'EMBOTT', 'EMBPHI', 'EMBPIT', 'EMBQUE', 'EMBSCUP', 'EMBSJ', 'EMBSTL', 'EMBTB', 'EMBTOR', 'EMBVAN', 'EMBVAN', 'EMBWES', 'EMBWPG', 'EMBWSH'],
   RINKPAL: ['RINK'],
   SUMMARY: ['SUMMARY2']
@@ -38,10 +38,22 @@ fs.readdir(root, (err, files) => {
           }
 
           let currentPalette = null;
-          for (const subFile of subFiles) { // check this folder for default palette files
-            if (subFile === '!pal' || subFile === '!p01' || subFile === '!pl1'|| subFile === '!pio') {
-              currentPalette = subFile;
-              break;
+          if (file in palLookup) {
+            const palFolder = palLookup[file];
+            const palFolderPath = path.join(root, palFolder);
+            const palFiles = fs.readdirSync(palFolderPath);
+            for (const palFile of palFiles) {
+              if (palFile === '!pal' || palFile === '!p01' || palFile === '!pl1' || palFile === '!pio') {
+                currentPalette = palFile;
+                break;
+              }
+            }
+          } else {
+            for (const subFile of subFiles) { // check this folder for default palette files
+              if (subFile === '!pal' || subFile === '!p01' || subFile === '!pl1' || subFile === '!pio') {
+                currentPalette = subFile;
+                break;
+              }
             }
           }
           console.log('AA TEST!!!',currentPalette);
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# EA-NHL-Tools 0.4
+# EA-NHL-Tools 0.4 ALPHA
 Tools for various EA NHL games
 
+Recommend checking out Releases for latest stable release: https://github.com/abdulahmad/EA-NHL-Tools/releases
+
 ## Tools included
-`PPV-SPIT-Tools` - Extracts NHL95 PC `.PPV` files (in-game sprites)
+`PPV-To-BMP` - Extracts NHL95 PC `.PPV` files (in-game sprites), WIP: with ability to pass in .ACT palette
 
 `IFF-To-WAV` - Converts NHL95 PC `.IFF` files to `.WAV` (out of game music files)
 
@@ -10,7 +12,11 @@ Tools for various EA NHL games
 
 `VIV-To-WAV` - Converts NHL95 PC `.VIV` files to `.WAV` (menu & rink announcer sound)
 
-`BIN-To-ACT` - Converts NHL95 PC `<HOME/AWAY>PALS.BIN` files to `.ACT` (jersey palettes)
+`SPIT-To-BMP` - Converts NHL95 PC SPIT format files to `.RAW` & `.BMP`, WIP: with ability to pass in .ACT palette
+
+`PAL-To-ACT` - WIP: Convert NHL95 PC `!<palname>` & `<HOME/AWAY>PALS.bin` files to `.ACT`.
+
+`QFS-To-BMP` - WIP: Convert NHL95 PC `.QFS` files to `.BMP` using defined mapping to ensure correct palette is used
 
 ### Release Notes
 
@@ -24,5 +30,7 @@ Tools for various EA NHL games
 - First release of VIV-To-WAV
 - Moved gfxpak to common 3rd party tools folder
 
-### 0.4
-- First release of BIN-To-ACT
+### 0.4 ALPHA
+- Alpha release with inflight Palette Tools
+- Intend to split out `SPIT-To-BMP` from `PPV-To-BMP` for final 0.4 release
+- Intend to split out `QFS-To-BMP` from `PAL-To-ACT` for final 0.4 release
