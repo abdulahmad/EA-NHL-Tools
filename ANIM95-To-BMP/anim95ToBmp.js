393dc7c6e6266a99ea873f4c0d12309df7f6526f
Abdul-Haseeb Ahmad <abdul.ahmad@gmail.com>
2025-06-01 09:15:54 -0400

    fixed stock palettes as much as i could

--- ANIM95-To-BMP/anim95ToBmp.js
+++ ANIM95-To-BMP/anim95ToBmp.js
@@ -10,7 +10,7 @@ const ROM_CONFIG = {
     expectedSize: 0x200000, // 1 MB (adjust if 2 MB)
     addresses: {
       spaList: { start:0x5B1C, end: 0x76B0, length: 0xA }, // incorrect
-      paletteOffset: { start: 0xC8506 }, // 0x206E before spriteTiles
+      paletteOffset: { start: 0x14BEFA }, // 0x206E before spriteTiles
       spriteTiles: { start: 0xCA574, end: 0x131874 }, // 0x3398 tiles or 13028 tiles.
       // tile size is 0x67300 or 422656 bytes
       frameOffsets: { start: 0x1318F4, end: 0x132136 }, // 0x842 bytes
@@ -25,7 +25,7 @@ const ROM_CONFIG = {
     expectedSize: 0x200000, // 1 MB (adjust if 2 MB)
     addresses: {
       spaList: { start:0x5B1C, end: 0x76B0, length: 0xA }, // incorrect
-      paletteOffset: { start: 0x172DE4 },
+      paletteOffset: { start: 0x172DF6 },
       spriteTiles: { start: 0x9AAA8, end: 0x12F588 },
       frameOffsets: { start: 0x12F608, end: 0x13012E }, // 0xB26 bytes
       // first sprite offset = 3718 , last sprite offset = 32750, diff of 29032 or 0x7168
@@ -40,7 +40,7 @@ const ROM_CONFIG = {
     addresses: {
       //spaList: { start:0x5B1C, end: 0x76B2 },
       spaList: { start:0x5B1C, end: 0x76B0, length: 0xA }, // incorrect
-      paletteOffset: { start: 0x172DE4 },
+      paletteOffset: { start: 0x9AF24 },
       spriteTiles: { start: 0xAABB8, end: 0x160CD8 },
       frameOffsets: { start: 0x160D58, end: 0x161D2E }, // 0xB26 bytes B61AA + AABB8 = 160D62
       // 160D58 + 4054
