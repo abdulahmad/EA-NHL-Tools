7a962e59baaf0ca5d1a7c487f205b64e08bf9a09
Abdul-Haseeb Ahmad <abdul.ahmad@gmail.com>
2025-06-20 17:32:49 -0400

    WIP

--- JIM-Tools/uncompress-jzip.js
+++ JIM-Tools/uncompress-jzip.js
@@ -212,7 +212,9 @@ class TileDecompressor {
                 };
             case 0x9: // Back reference with byte offset (alternative)
                 // const backRefCountAlt = (param*2) + 1 + 2; // added additional 2 bytes-- question if this should be in or out of bracket
-                const backRefCountAlt = (3*param) - 8;
+                // const backRefCountAlt = (3*param) - 8;
+                // const backRefCountAlt = 6 * Math.floor(param / 4) + (param % 4) + 10;
+                const backRefCountAlt = 2 * param + Math.floor(param / 4) + 1;
                 // B --> (11*2) + 1 + 2 = 22 + 3 = 25
                 // B -> 1011
                 // C --> (12*2) + 1 + 2 = 24 + 3 = 27 -> should be 28
